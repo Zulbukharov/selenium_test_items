78ad0523cb766a2191407f23360c01298449d322
Abylaikhan Zulbukharov <l1pt0n1905@gmail.com>
2019-06-19 01:14:57 +0600

    [edit][fix links readme]

--- test_items.py
+++ test_items.py
@@ -7,4 +7,5 @@ def test_item_add_to_basket(browser):
     # time.sleep(30)
     basket = browser.find_element_by_css_selector(
         "form button.btn-add-to-basket")
+    assert len(basket) > 0, "Element not found"
     print(basket.text)
